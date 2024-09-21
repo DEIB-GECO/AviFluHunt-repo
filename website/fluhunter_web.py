347b441ef6d048558e02c8f022c45c266fdb9afd
nixetheus <luca.cassenti.00@gmail.com>
2024-09-21 11:26:00 +0200

    Added password check

--- website/fluhunter_web.py
+++ website/fluhunter_web.py
@@ -1,3 +1,5 @@
+import hmac
+
 import pandas as pd
 from duckdb.duckdb import aggregate
 
@@ -9,6 +11,35 @@ st.set_page_config(layout="wide")
 
 # DATABASE
 db = st.connection(name="thesis", type="sql", url="sqlite:///website/data/thesis.db")
+
+
+# PASSWORD CHECK
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 # FRONTEND
 N_QUERIES = 15
