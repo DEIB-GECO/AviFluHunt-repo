a15e321c51192c159c3272370ad8a3fd941c4088
nixetheus <luca.cassenti.00@gmail.com>
2024-09-26 13:14:26 +0200

    backend docker

---
+++ docker_config/backend/backend.sh
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env bash
+
+exec python3 database/handler.py & 
+exec python3 resources/domain_data/insert_domain.py &  # may need files for ref and subtypes
+exec python3 resources/academic_data/insert_academic.py &  # may need files for effects, markers and papers
+exec python3 resources/academic_data/insert_markers.py &
+exec python3 insert_segments.py & # needs files (fasta + metadata)
+#exec python3 insert_mutations.py  # needs files
+/bin/bash -it
