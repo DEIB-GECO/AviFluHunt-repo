bf02626aaf6422f955473f1399761348dfedb932
nixetheus <luca.cassenti.00@gmail.com>
2025-07-27 17:13:32 +0200

    Small changes and fixes

--- website/query_functions.py
+++ website/query_functions.py
@@ -269,9 +269,11 @@ def params10():
         bin_size = st.number_input(label=strings["param_label10c"], min_value=0, step=10, value=0)
         offset = st.number_input(label=strings["param_label10d"], min_value=0, step=1, value=0)
 
-    if st.session_state.num_inputs > 0 or bin_size == 0:
-        bins = [(st.session_state[f"start_{i + 1}"], st.session_state[f"end_{i + 1}"])
-                for i in range(st.session_state.num_inputs)]
+    if bin_size == 0:
+        bins = [(0, 0)]
+        if st.session_state.num_inputs > 0:
+            bins = [(st.session_state[f"start_{i + 1}"], st.session_state[f"end_{i + 1}"])
+                    for i in range(st.session_state.num_inputs)]
     else:
         bins = [(i, i + bin_size) for i in range(offset, 1000 + offset, bin_size)][0:500]
 
